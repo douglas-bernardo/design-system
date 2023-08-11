d9102d5cfdf5959bb756874ed9d1269d57b0b13f
Jackson Douglas <jkdouglas21@gmail.com>
2023-08-11 20:32:49 -0300

    Set some storybook improvments

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/douglas-bernardo.png',
     alt: 'Diego Fernandes',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
