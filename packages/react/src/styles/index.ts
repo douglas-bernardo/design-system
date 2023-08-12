8f3deeb1db63bd0bd1dbd3601650b2dda35b335f
Jackson Douglas <jkdouglas21@gmail.com>
2023-08-12 15:43:56 -0300

    fix: update project ui package name

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@doug-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
